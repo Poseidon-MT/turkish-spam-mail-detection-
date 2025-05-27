efe5c20d646b1a53e3b223984cab9cbc25bf2863
Mehmet Topal <poseidon07mt@gmail.com>
2025-05-27 11:39:13 +0300

    model update

--- model_loader.py
+++ model_loader.py
@@ -2,7 +2,7 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
 
 def load_model():
-    model_adi = "poseidon07x/bert-spam-model"
+    model_adi = "poseidon07x/bert-base-multilingual-cased-spam-mail-detection"
     model = AutoModelForSequenceClassification.from_pretrained(model_adi)
     tokenizer = AutoTokenizer.from_pretrained(model_adi)
     pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer)
